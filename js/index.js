50edf656a726e0516fe80dd2c10d45d20ae405be
simran <simransingh956429@gmail.com>
2024-12-12 19:46:07 +0530

    basics

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 let a=1;
 a="coding techniques";
+
+function add(a,b){   //no type checking 
+    return a+b;
+}
--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,17 @@
-let a1=1;
+let a1: number = 1;        //syntax for type
 //a1="coding techniques";  //error //statically typed lang
 a1=10; //no error
+
+let a2: string ="hi"   //type-string
+
+
+//syntax for function and mentioning type
+
+function add1 (a : number , b : number ) : number { 
+
+    return a + b;   //type checking
+}
+
+add1(1,2);
+
+
